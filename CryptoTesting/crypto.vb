c9a9751b8a77381027df785c07047ffcf04bc864
nliaquin <nliaquin@svsu.edu>
2020-10-01 22:11:41 -0400

    Finished the program: -Added a formatting module -Refactored the Crypto object into a simple module -Fixed UI bugs

---
+++ CryptoTesting/crypto.vb
@@ -0,0 +1,62 @@
+﻿Imports System.Security.Cryptography
+
+Module crypto
+    Private CSP As New TripleDESCryptoServiceProvider
+    Dim sha1 As New SHA1CryptoServiceProvider
+    Dim sha256 As New SHA256CryptoServiceProvider
+    Dim sha512 As New SHA512CryptoServiceProvider
+
+    Private Function Truncate(ByVal key As String, ByVal sha As String, ByVal length As Integer) As Byte()
+        Dim thisSha As Object
+
+        Select Case sha
+            Case "SHA1"
+                thisSha = New SHA1CryptoServiceProvider
+
+            Case "SHA256"
+                thisSha = New SHA256CryptoServiceProvider
+
+            Case "SHA512"
+                thisSha = New SHA512CryptoServiceProvider
+        End Select
+
+        Dim keyBytes() As Byte = Text.Encoding.Unicode.GetBytes(key)
+        Dim hash() As Byte = thisSha.ComputeHash(keyBytes)
+
+        ReDim Preserve hash(length - 1)
+        Return hash
+    End Function
+
+    Sub InitializeCrypto(ByVal key As String, ByVal sha As String)
+        CSP.Key = Truncate(key, sha, CSP.KeySize \ 8)
+        CSP.IV = Truncate("", sha, CSP.BlockSize \ 8)
+    End Sub
+
+    Public Function EncryptData(ByVal plainText As String) As String
+        Dim plainTextBytes() As Byte = Text.Encoding.Unicode.GetBytes(plainText)
+
+        Dim memStream As New IO.MemoryStream
+
+        Dim encStream As New CryptoStream(memStream, CSP.CreateEncryptor(), CryptoStreamMode.Write)
+
+        encStream.Write(plainTextBytes, 0, plainTextBytes.Length)
+
+        encStream.FlushFinalBlock()
+
+        Return Convert.ToBase64String(memStream.ToArray)
+    End Function
+
+    Public Function DecryptData(ByVal encryptedText As String) As String
+        Dim encryptedBytes() As Byte = Convert.FromBase64String(encryptedText)
+
+        Dim memStream As New IO.MemoryStream
+
+        Dim decStream As New CryptoStream(memStream, CSP.CreateDecryptor(), CryptoStreamMode.Write)
+
+        decStream.Write(encryptedBytes, 0, encryptedBytes.Length)
+
+        decStream.FlushFinalBlock()
+
+        Return Text.Encoding.Unicode.GetString(memStream.ToArray)
+    End Function
+End Module
--- CryptoTesting/frmMain.Designer.vb
+++ CryptoTesting/frmMain.Designer.vb
@@ -23,40 +23,38 @@ Partial Class frmMain
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.grpLogin = New System.Windows.Forms.GroupBox()
-        Me.lblDesc = New System.Windows.Forms.Label()
-        Me.lblKey = New System.Windows.Forms.Label()
-        Me.txtKey = New System.Windows.Forms.TextBox()
-        Me.lblHash = New System.Windows.Forms.Label()
-        Me.cbHash = New System.Windows.Forms.ComboBox()
-        Me.btnCancel = New System.Windows.Forms.Button()
         Me.btnContinue = New System.Windows.Forms.Button()
+        Me.cbHash = New System.Windows.Forms.ComboBox()
+        Me.lblHash = New System.Windows.Forms.Label()
+        Me.txtKey = New System.Windows.Forms.TextBox()
+        Me.lblKey = New System.Windows.Forms.Label()
+        Me.lblDesc = New System.Windows.Forms.Label()
         Me.grpEncryption = New System.Windows.Forms.GroupBox()
-        Me.lblValKey = New System.Windows.Forms.Label()
-        Me.lblValHash = New System.Windows.Forms.Label()
-        Me.valKey = New System.Windows.Forms.Label()
-        Me.valHash = New System.Windows.Forms.Label()
-        Me.grpPlain2Encrypted = New System.Windows.Forms.GroupBox()
         Me.grpEncrypted2Plain = New System.Windows.Forms.GroupBox()
-        Me.lblFromPlain = New System.Windows.Forms.Label()
-        Me.lblToEncrypted = New System.Windows.Forms.Label()
-        Me.txtFromPlain = New System.Windows.Forms.TextBox()
-        Me.txtToEncrypted = New System.Windows.Forms.TextBox()
-        Me.cmdEncrypt = New System.Windows.Forms.Button()
         Me.cmdDecrypt = New System.Windows.Forms.Button()
         Me.txtFromEncrypted = New System.Windows.Forms.TextBox()
         Me.txtToPlain = New System.Windows.Forms.TextBox()
-        Me.lblFromEncrypted = New System.Windows.Forms.Label()
         Me.lblToPlain = New System.Windows.Forms.Label()
+        Me.lblFromEncrypted = New System.Windows.Forms.Label()
+        Me.grpPlain2Encrypted = New System.Windows.Forms.GroupBox()
+        Me.cmdEncrypt = New System.Windows.Forms.Button()
+        Me.txtToEncrypted = New System.Windows.Forms.TextBox()
+        Me.txtFromPlain = New System.Windows.Forms.TextBox()
+        Me.lblToEncrypted = New System.Windows.Forms.Label()
+        Me.lblFromPlain = New System.Windows.Forms.Label()
+        Me.valHash = New System.Windows.Forms.Label()
+        Me.valKey = New System.Windows.Forms.Label()
+        Me.lblValHash = New System.Windows.Forms.Label()
+        Me.lblValKey = New System.Windows.Forms.Label()
         Me.grpLogin.SuspendLayout()
         Me.grpEncryption.SuspendLayout()
-        Me.grpPlain2Encrypted.SuspendLayout()
         Me.grpEncrypted2Plain.SuspendLayout()
+        Me.grpPlain2Encrypted.SuspendLayout()
         Me.SuspendLayout()
         '
         'grpLogin
         '
         Me.grpLogin.Controls.Add(Me.btnContinue)
-        Me.grpLogin.Controls.Add(Me.btnCancel)
         Me.grpLogin.Controls.Add(Me.cbHash)
         Me.grpLogin.Controls.Add(Me.lblHash)
         Me.grpLogin.Controls.Add(Me.txtKey)
@@ -67,7 +65,49 @@ Partial Class frmMain
         Me.grpLogin.Size = New System.Drawing.Size(455, 158)
         Me.grpLogin.TabIndex = 0
         Me.grpLogin.TabStop = False
-        Me.grpLogin.Text = "Key Setup"
+        '
+        'btnContinue
+        '
+        Me.btnContinue.Location = New System.Drawing.Point(134, 129)
+        Me.btnContinue.Name = "btnContinue"
+        Me.btnContinue.Size = New System.Drawing.Size(187, 23)
+        Me.btnContinue.TabIndex = 6
+        Me.btnContinue.Text = "Continue"
+        Me.btnContinue.UseVisualStyleBackColor = True
+        '
+        'cbHash
+        '
+        Me.cbHash.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbHash.Items.AddRange(New Object() {"SHA1", "SHA256", "SHA512"})
+        Me.cbHash.Location = New System.Drawing.Point(49, 96)
+        Me.cbHash.Name = "cbHash"
+        Me.cbHash.Size = New System.Drawing.Size(382, 21)
+        Me.cbHash.TabIndex = 4
+        '
+        'lblHash
+        '
+        Me.lblHash.AutoSize = True
+        Me.lblHash.Location = New System.Drawing.Point(8, 99)
+        Me.lblHash.Name = "lblHash"
+        Me.lblHash.Size = New System.Drawing.Size(35, 13)
+        Me.lblHash.TabIndex = 3
+        Me.lblHash.Text = "Hash:"
+        '
+        'txtKey
+        '
+        Me.txtKey.Location = New System.Drawing.Point(49, 67)
+        Me.txtKey.Name = "txtKey"
+        Me.txtKey.Size = New System.Drawing.Size(382, 20)
+        Me.txtKey.TabIndex = 2
+        '
+        'lblKey
+        '
+        Me.lblKey.AutoSize = True
+        Me.lblKey.Location = New System.Drawing.Point(15, 70)
+        Me.lblKey.Name = "lblKey"
+        Me.lblKey.Size = New System.Drawing.Size(28, 13)
+        Me.lblKey.TabIndex = 1
+        Me.lblKey.Text = "Key:"
         '
         'lblDesc
         '
@@ -80,57 +120,6 @@ Partial Class frmMain
     "Keys are used to secure hashes. Please also select the hash you would like to te" &
     "st."
         Me.lblDesc.TextAlign = System.Drawing.ContentAlignment.TopCenter
-        '
-        'lblKey
-        '
-        Me.lblKey.AutoSize = True
-        Me.lblKey.Location = New System.Drawing.Point(15, 70)
-        Me.lblKey.Name = "lblKey"
-        Me.lblKey.Size = New System.Drawing.Size(28, 13)
-        Me.lblKey.TabIndex = 1
-        Me.lblKey.Text = "Key:"
-        '
-        'txtKey
-        '
-        Me.txtKey.Location = New System.Drawing.Point(49, 67)
-        Me.txtKey.Name = "txtKey"
-        Me.txtKey.Size = New System.Drawing.Size(382, 20)
-        Me.txtKey.TabIndex = 2
-        '
-        'lblHash
-        '
-        Me.lblHash.AutoSize = True
-        Me.lblHash.Location = New System.Drawing.Point(8, 99)
-        Me.lblHash.Name = "lblHash"
-        Me.lblHash.Size = New System.Drawing.Size(35, 13)
-        Me.lblHash.TabIndex = 3
-        Me.lblHash.Text = "Hash:"
-        '
-        'cbHash
-        '
-        Me.cbHash.Items.AddRange(New Object() {"SHA1", "SHA256", "SHA516"})
-        Me.cbHash.Location = New System.Drawing.Point(49, 96)
-        Me.cbHash.Name = "cbHash"
-        Me.cbHash.Size = New System.Drawing.Size(382, 21)
-        Me.cbHash.TabIndex = 4
-        '
-        'btnCancel
-        '
-        Me.btnCancel.Location = New System.Drawing.Point(116, 129)
-        Me.btnCancel.Name = "btnCancel"
-        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
-        Me.btnCancel.TabIndex = 5
-        Me.btnCancel.Text = "Cancel"
-        Me.btnCancel.UseVisualStyleBackColor = True
-        '
-        'btnContinue
-        '
-        Me.btnContinue.Location = New System.Drawing.Point(272, 129)
-        Me.btnContinue.Name = "btnContinue"
-        Me.btnContinue.Size = New System.Drawing.Size(75, 23)
-        Me.btnContinue.TabIndex = 6
-        Me.btnContinue.Text = "Continue"
-        Me.btnContinue.UseVisualStyleBackColor = True
         '
         'grpEncryption
         '
@@ -145,56 +134,7 @@ Partial Class frmMain
         Me.grpEncryption.Size = New System.Drawing.Size(573, 304)
         Me.grpEncryption.TabIndex = 1
         Me.grpEncryption.TabStop = False
-        '
-        'lblValKey
-        '
-        Me.lblValKey.AutoSize = True
-        Me.lblValKey.Location = New System.Drawing.Point(22, 27)
-        Me.lblValKey.Name = "lblValKey"
-        Me.lblValKey.Size = New System.Drawing.Size(28, 13)
-        Me.lblValKey.TabIndex = 0
-        Me.lblValKey.Text = "Key:"
-        '
-        'lblValHash
-        '
-        Me.lblValHash.AutoSize = True
-        Me.lblValHash.Location = New System.Drawing.Point(15, 51)
-        Me.lblValHash.Name = "lblValHash"
-        Me.lblValHash.Size = New System.Drawing.Size(35, 13)
-        Me.lblValHash.TabIndex = 1
-        Me.lblValHash.Text = "Hash:"
-        '
-        'valKey
-        '
-        Me.valKey.AutoSize = True
-        Me.valKey.Location = New System.Drawing.Point(56, 27)
-        Me.valKey.Name = "valKey"
-        Me.valKey.Size = New System.Drawing.Size(24, 13)
-        Me.valKey.TabIndex = 2
-        Me.valKey.Text = "key"
-        '
-        'valHash
-        '
-        Me.valHash.AutoSize = True
-        Me.valHash.Location = New System.Drawing.Point(56, 51)
-        Me.valHash.Name = "valHash"
-        Me.valHash.Size = New System.Drawing.Size(30, 13)
-        Me.valHash.TabIndex = 3
-        Me.valHash.Text = "hash"
-        '
-        'grpPlain2Encrypted
-        '
-        Me.grpPlain2Encrypted.Controls.Add(Me.cmdEncrypt)
-        Me.grpPlain2Encrypted.Controls.Add(Me.txtToEncrypted)
-        Me.grpPlain2Encrypted.Controls.Add(Me.txtFromPlain)
-        Me.grpPlain2Encrypted.Controls.Add(Me.lblToEncrypted)
-        Me.grpPlain2Encrypted.Controls.Add(Me.lblFromPlain)
-        Me.grpPlain2Encrypted.Location = New System.Drawing.Point(11, 73)
-        Me.grpPlain2Encrypted.Name = "grpPlain2Encrypted"
-        Me.grpPlain2Encrypted.Size = New System.Drawing.Size(556, 103)
-        Me.grpPlain2Encrypted.TabIndex = 4
-        Me.grpPlain2Encrypted.TabStop = False
-        Me.grpPlain2Encrypted.Text = "Plain Text to Encrypted Text"
+        Me.grpEncryption.Visible = False
         '
         'grpEncrypted2Plain
         '
@@ -209,47 +149,6 @@ Partial Class frmMain
         Me.grpEncrypted2Plain.TabIndex = 5
         Me.grpEncrypted2Plain.TabStop = False
         Me.grpEncrypted2Plain.Text = "Encrypted Text to Plain Text"
-        '
-        'lblFromPlain
-        '
-        Me.lblFromPlain.AutoSize = True
-        Me.lblFromPlain.Location = New System.Drawing.Point(31, 22)
-        Me.lblFromPlain.Name = "lblFromPlain"
-        Me.lblFromPlain.Size = New System.Drawing.Size(33, 13)
-        Me.lblFromPlain.TabIndex = 0
-        Me.lblFromPlain.Text = "Plain:"
-        '
-        'lblToEncrypted
-        '
-        Me.lblToEncrypted.AutoSize = True
-        Me.lblToEncrypted.Location = New System.Drawing.Point(6, 48)
-        Me.lblToEncrypted.Name = "lblToEncrypted"
-        Me.lblToEncrypted.Size = New System.Drawing.Size(58, 13)
-        Me.lblToEncrypted.TabIndex = 1
-        Me.lblToEncrypted.Text = "Encrypted:"
-        '
-        'txtFromPlain
-        '
-        Me.txtFromPlain.Location = New System.Drawing.Point(70, 19)
-        Me.txtFromPlain.Name = "txtFromPlain"
-        Me.txtFromPlain.Size = New System.Drawing.Size(480, 20)
-        Me.txtFromPlain.TabIndex = 2
-        '
-        'txtToEncrypted
-        '
-        Me.txtToEncrypted.Location = New System.Drawing.Point(70, 45)
-        Me.txtToEncrypted.Name = "txtToEncrypted"
-        Me.txtToEncrypted.Size = New System.Drawing.Size(480, 20)
-        Me.txtToEncrypted.TabIndex = 3
-        '
-        'cmdEncrypt
-        '
-        Me.cmdEncrypt.Location = New System.Drawing.Point(16, 71)
-        Me.cmdEncrypt.Name = "cmdEncrypt"
-        Me.cmdEncrypt.Size = New System.Drawing.Size(525, 23)
-        Me.cmdEncrypt.TabIndex = 4
-        Me.cmdEncrypt.Text = "Encrypt Plain Text Entry"
-        Me.cmdEncrypt.UseVisualStyleBackColor = True
         '
         'cmdDecrypt
         '
@@ -274,15 +173,6 @@ Partial Class frmMain
         Me.txtToPlain.Size = New System.Drawing.Size(480, 20)
         Me.txtToPlain.TabIndex = 7
         '
-        'lblFromEncrypted
-        '
-        Me.lblFromEncrypted.AutoSize = True
-        Me.lblFromEncrypted.Location = New System.Drawing.Point(6, 22)
-        Me.lblFromEncrypted.Name = "lblFromEncrypted"
-        Me.lblFromEncrypted.Size = New System.Drawing.Size(58, 13)
-        Me.lblFromEncrypted.TabIndex = 6
-        Me.lblFromEncrypted.Text = "Encrypted:"
-        '
         'lblToPlain
         '
         Me.lblToPlain.AutoSize = True
@@ -292,30 +182,131 @@ Partial Class frmMain
         Me.lblToPlain.TabIndex = 5
         Me.lblToPlain.Text = "Plain:"
         '
+        'lblFromEncrypted
+        '
+        Me.lblFromEncrypted.AutoSize = True
+        Me.lblFromEncrypted.Location = New System.Drawing.Point(6, 22)
+        Me.lblFromEncrypted.Name = "lblFromEncrypted"
+        Me.lblFromEncrypted.Size = New System.Drawing.Size(58, 13)
+        Me.lblFromEncrypted.TabIndex = 6
+        Me.lblFromEncrypted.Text = "Encrypted:"
+        '
+        'grpPlain2Encrypted
+        '
+        Me.grpPlain2Encrypted.Controls.Add(Me.cmdEncrypt)
+        Me.grpPlain2Encrypted.Controls.Add(Me.txtToEncrypted)
+        Me.grpPlain2Encrypted.Controls.Add(Me.txtFromPlain)
+        Me.grpPlain2Encrypted.Controls.Add(Me.lblToEncrypted)
+        Me.grpPlain2Encrypted.Controls.Add(Me.lblFromPlain)
+        Me.grpPlain2Encrypted.Location = New System.Drawing.Point(11, 73)
+        Me.grpPlain2Encrypted.Name = "grpPlain2Encrypted"
+        Me.grpPlain2Encrypted.Size = New System.Drawing.Size(556, 103)
+        Me.grpPlain2Encrypted.TabIndex = 4
+        Me.grpPlain2Encrypted.TabStop = False
+        Me.grpPlain2Encrypted.Text = "Plain Text to Encrypted Text"
+        '
+        'cmdEncrypt
+        '
+        Me.cmdEncrypt.Location = New System.Drawing.Point(16, 71)
+        Me.cmdEncrypt.Name = "cmdEncrypt"
+        Me.cmdEncrypt.Size = New System.Drawing.Size(525, 23)
+        Me.cmdEncrypt.TabIndex = 4
+        Me.cmdEncrypt.Text = "Encrypt Plain Text Entry"
+        Me.cmdEncrypt.UseVisualStyleBackColor = True
+        '
+        'txtToEncrypted
+        '
+        Me.txtToEncrypted.Location = New System.Drawing.Point(70, 45)
+        Me.txtToEncrypted.Name = "txtToEncrypted"
+        Me.txtToEncrypted.Size = New System.Drawing.Size(480, 20)
+        Me.txtToEncrypted.TabIndex = 3
+        '
+        'txtFromPlain
+        '
+        Me.txtFromPlain.Location = New System.Drawing.Point(70, 19)
+        Me.txtFromPlain.Name = "txtFromPlain"
+        Me.txtFromPlain.Size = New System.Drawing.Size(480, 20)
+        Me.txtFromPlain.TabIndex = 2
+        '
+        'lblToEncrypted
+        '
+        Me.lblToEncrypted.AutoSize = True
+        Me.lblToEncrypted.Location = New System.Drawing.Point(6, 48)
+        Me.lblToEncrypted.Name = "lblToEncrypted"
+        Me.lblToEncrypted.Size = New System.Drawing.Size(58, 13)
+        Me.lblToEncrypted.TabIndex = 1
+        Me.lblToEncrypted.Text = "Encrypted:"
+        '
+        'lblFromPlain
+        '
+        Me.lblFromPlain.AutoSize = True
+        Me.lblFromPlain.Location = New System.Drawing.Point(31, 22)
+        Me.lblFromPlain.Name = "lblFromPlain"
+        Me.lblFromPlain.Size = New System.Drawing.Size(33, 13)
+        Me.lblFromPlain.TabIndex = 0
+        Me.lblFromPlain.Text = "Plain:"
+        '
+        'valHash
+        '
+        Me.valHash.AutoSize = True
+        Me.valHash.Location = New System.Drawing.Point(56, 51)
+        Me.valHash.Name = "valHash"
+        Me.valHash.Size = New System.Drawing.Size(30, 13)
+        Me.valHash.TabIndex = 3
+        Me.valHash.Text = "hash"
+        '
+        'valKey
+        '
+        Me.valKey.AutoSize = True
+        Me.valKey.Location = New System.Drawing.Point(56, 27)
+        Me.valKey.Name = "valKey"
+        Me.valKey.Size = New System.Drawing.Size(24, 13)
+        Me.valKey.TabIndex = 2
+        Me.valKey.Text = "key"
+        '
+        'lblValHash
+        '
+        Me.lblValHash.AutoSize = True
+        Me.lblValHash.Location = New System.Drawing.Point(15, 51)
+        Me.lblValHash.Name = "lblValHash"
+        Me.lblValHash.Size = New System.Drawing.Size(35, 13)
+        Me.lblValHash.TabIndex = 1
+        Me.lblValHash.Text = "Hash:"
+        '
+        'lblValKey
+        '
+        Me.lblValKey.AutoSize = True
+        Me.lblValKey.Location = New System.Drawing.Point(22, 27)
+        Me.lblValKey.Name = "lblValKey"
+        Me.lblValKey.Size = New System.Drawing.Size(28, 13)
+        Me.lblValKey.TabIndex = 0
+        Me.lblValKey.Text = "Key:"
+        '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(623, 508)
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(598, 488)
         Me.Controls.Add(Me.grpEncryption)
         Me.Controls.Add(Me.grpLogin)
+        Me.MaximizeBox = False
         Me.Name = "frmMain"
         Me.Text = "Crypto Testing"
         Me.grpLogin.ResumeLayout(False)
         Me.grpLogin.PerformLayout()
         Me.grpEncryption.ResumeLayout(False)
         Me.grpEncryption.PerformLayout()
-        Me.grpPlain2Encrypted.ResumeLayout(False)
-        Me.grpPlain2Encrypted.PerformLayout()
         Me.grpEncrypted2Plain.ResumeLayout(False)
         Me.grpEncrypted2Plain.PerformLayout()
+        Me.grpPlain2Encrypted.ResumeLayout(False)
+        Me.grpPlain2Encrypted.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents grpLogin As Windows.Forms.GroupBox
     Friend WithEvents btnContinue As Windows.Forms.Button
-    Friend WithEvents btnCancel As Windows.Forms.Button
     Friend WithEvents cbHash As Windows.Forms.ComboBox
     Friend WithEvents lblHash As Windows.Forms.Label
     Friend WithEvents txtKey As Windows.Forms.TextBox
